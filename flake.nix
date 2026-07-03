ab09226226b6c02fce27d630ce61ada72232b205
Jeremy Gao <wensimehrp@gmail.com>
2026-07-03 04:39:30 -0700

    chore: update flakes and typst version

--- flake.nix
+++ flake.nix
@@ -17,39 +17,9 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-
-        # this would be removed once Typst reaches 0.15, or 1.0, or something else
-        typst = pkgs.rustPlatform.buildRustPackage rec {
-          pname = "typst";
-          version = "git";
-
-          src = pkgs.fetchFromGitHub {
-            owner = "typst";
-            repo = "typst";
-            rev = "64720d459129f4e5561c3bd0519bdfaef034b208";
-            sha256 = "sha256-sGPlHS64zuKtMp5XqjLGUgDcnCe5sA1oMeNRLHLK0BE=";
-          };
-
-          cargoLock.lockFile = src + "/Cargo.lock";
-          cargoLock.outputHashes = {
-            "codex-0.2.0" = "sha256-yPhL3yV9R9qUjJ3nqfUY99hoqwwGGdZ4HbpdcspXbrk=";
-            "krilla-0.6.0" = "sha256-DW0l6radzJ99JJPdE/O5RT747/BHH1bv94vtgBUO2N0=";
-            "typst-assets-0.14.2" = "sha256-rt/4/NAdfxfxMjzkAsDGCYofUz+dh92gOFdvHNcut8w=";
-            "typst-dev-assets-0.14.2" = "sha256-2GGAoFVa87eO55MqahX+8Dg4hTwIqqIrnhZZcl9ACO4=";
-          };
-
-          nativeBuildInputs = [
-            pkgs.pkg-config
-          ];
-
-          buildInputs = [
-            pkgs.openssl
-          ];
-        };
-
       in
       {
-        devShells.default = pkgs.mkShell { packages = [ typst ]; };
+        devShells.default = pkgs.mkShell { packages = [ pkgs.typst ]; };
       }
     );
 }
